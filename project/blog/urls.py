a0f0158eab229a4121142c46fcedfad91d08a5d0
Gourav629 <zackroz5@gmail.com>
2023-04-04 01:32:32 +0530

    Course Linking

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/',views.Index),
-    path('home/',views.Home),
-    path('temp/',views.Temp),
-    path('about/',views.About),
-    path('abc/',views.Abc),
-    path('xyz/',views.Xyz),
     path('',views.Blog),
+    path('about/',views.About),
+    path('home/',views.Home),
+    path('xyz/',views.Xyz),
+    path('inter/',views.Interview),
+    path('inter_viewer/',views.InterViewers),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -3,31 +3,32 @@ from django.http import HttpResponse
 from templates import *
 # Create your views here.
 
-def Index(request):
-    return render(request, 'index.html')
-
-def Home(request):
-    return render(request, 'home.html')
-
-def Temp(request):
-    return render(request, 'temp.html')
 
 def About(request):
     return render(request, 'about.html')
 
-def Abc(request):
+def Home(request):
+    return render(request, 'home.html')
+
+def Interview(request):
+    return render(request, 'interviews.html')
+
+def InterViewers(request):
+    return render(request, 'interviews.html')
+
+def Blog(request):
     context = {
-        'data':["1.jpeg","2.jpeg","3.jpeg","5.jpeg"],
-        'carosel':[
-        ("teaching.jpeg","Reading is a good habit","All great leaders are good readers.",True),
-        ("social.jpeg","So many books , so little time","The more you practice, the more you'll become good at it",False),
-        ("emotions.jpeg","Writing is an art","Books are your best friend. Spend most time with them",False),
-        ]
+        "data":[("World","Featured post","Nov 12","This is a wider card with supporting text below as a natural lead-in to additional content","https://source.unsplash.com/1400x400/?teaching","inter_viewer"),("Open Access , Publishing","Katharine Dunn: Open access is still viewed as a bold step—when it should be the norm","July 18 2022","Dissemination of knowledge” is a core value that people return to again and again when making decisions around open sharing—including during the creation of OCW, DSpace, and MIT’s faculty OA policy.","https://openinterview.org/wp-content/uploads/2022/07/Katharine_Dunn-Open_Interview-MIT-Santosh_C_Hulagabali-removebg-preview.png","inter_viewer"),("Avinash","Gourav","Abhishek","Manish","https://source.unsplash.com/1400x400/?emotions","inter_viewer"),("Avinash","Gourav","Manish","Abhishek","https://source.unsplash.com/1400x400/?teaching","inter_viewer")],
+        "arch":["January","February","March","April","June","July","August","September","Octorber","November","December"],
+        "caro":[("https://source.unsplash.com/1400x400/?teaching",True),("https://source.unsplash.com/1400x400/?emotions",False),("https://source.unsplash.com/1400x400/?social,learning",False)],
+        "images":["https://source.unsplash.com/1400x400/?teaching","https://source.unsplash.com/1400x400/?emotions","https://source.unsplash.com/1400x400/?social,learning"],
+        "about":'/about',
+        "index":'/',
+        "inter":'/inter',
+        "interviewer":'/inter_viewer',
+        "home":'/home',
     }
     return render(request, 'nbase.html',context)
 
 def Xyz(request):
     return render(request, 'xyz.html')
-
-def Blog(request):
-    return render(request, 'blog/nbase.html',{'title':'Blog','cname':'Blog','data':["1.jpeg","2.jpeg","3.jpeg","5.jpeg"]})
